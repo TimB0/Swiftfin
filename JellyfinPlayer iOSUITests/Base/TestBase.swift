200582d6f3f8960a871727dae010432bf34ad653
TimB0 <tboland1@gmail.com>
2021-09-02 15:16:28 -0700

    a little cleanup

--- JellyfinPlayer iOSUITests/Base/TestBase.swift
+++ JellyfinPlayer iOSUITests/Base/TestBase.swift
@@ -24,11 +24,11 @@ class TestBase: XCTestCase {
         super.tearDown()
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-             measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//             measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
