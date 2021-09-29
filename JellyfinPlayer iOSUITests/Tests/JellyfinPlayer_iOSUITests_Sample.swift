4519408972c91073f76ae2a8a4535fa86e974057
TimB0 <tboland1@gmail.com>
2021-09-29 16:00:24 -0700

    uncommenting sample test

--- JellyfinPlayer iOSUITests/Tests/JellyfinPlayer_iOSUITests_Sample.swift
+++ JellyfinPlayer iOSUITests/Tests/JellyfinPlayer_iOSUITests_Sample.swift
@@ -1,15 +1,15 @@
-//
+
  /* 
   * SwiftFin is subject to the terms of the Mozilla Public
   * License, v2.0. If a copy of the MPL was not distributed with this
-  * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+  * file, you can obtain one at https:mozilla.org/MPL/2.0/.
   *
   * Copyright 2021 Timothy Boland & Jellyfin Contributors
   */
 
 
 
-/*
+
 import XCTest
 
 class JellyfinPlayer_iOSUITests: TestBase {
@@ -18,9 +18,9 @@ class JellyfinPlayer_iOSUITests: TestBase {
         
         XCTAssertFalse(app.buttons["Connect"].isEnabled)
         app.staticTexts["DISCOVERED SERVERS"].tap()
-        app/*@START_MENU_TOKEN@*/.buttons["Connect"]/*[[".cells[\"Connect\"].buttons[\"Connect\"]",".buttons[\"Connect\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.buttons["Connect"].tap()
         XCTAssertFalse(app.buttons["Connect"].isEnabled)
-        app/*@START_MENU_TOKEN@*/.textFields["Server URL"]/*[[".cells[\"Server URL\"].textFields[\"Server URL\"]",".textFields[\"Server URL\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.textFields["Server URL"].tap()
         app.staticTexts["CONNECT MANUALLY"].tap()
         app.navigationBars["Connect to Server"].staticTexts["Connect to Server"].tap()
                 
@@ -28,8 +28,8 @@ class JellyfinPlayer_iOSUITests: TestBase {
     
     func testEnterServerAddress() {
         
-        app/*@START_MENU_TOKEN@*/.textFields["Server URL"]/*[[".cells[\"Server URL\"].textFields[\"Server URL\"]",".textFields[\"Server URL\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.textFields["Server URL"].typeText("https://demo.jellyfin.org/stable")
+        app.textFields["Server URL"].tap()
+        app.textFields["Server URL"].typeText("https:demo.jellyfin.org/stable")
         XCTAssertTrue(app.buttons["Connect"].isEnabled)
         app.buttons["Connect"].tap()
         app.staticTexts["LOGIN TO STABLE DEMO"].tap()
@@ -37,6 +37,6 @@ class JellyfinPlayer_iOSUITests: TestBase {
 
     }
 }
- */
+
  
 
